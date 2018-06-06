339445b1679cb7771003364783b775d702251401
Florian Keller <florian.keller@wire.com>
2018-06-06 15:29:32 +0200

    Add server tests

---
+++ spec/test/ServerSpec.js
@@ -0,0 +1,73 @@
+/*
+ * Wire
+ * Copyright (C) 2018 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+/* eslint no-magic-numbers: "off" */
+// @ts-check
+
+require('dotenv').config({path: 'spec/test/.env'});
+const {default: config} = require('../../dist/config');
+const request = require('request');
+const {default: Server} = require('../../dist/Server');
+
+describe('Server', () => {
+  let server;
+
+  beforeEach(() => (server = new Server(config)));
+
+  afterEach(async done => {
+    if (server && server.server) {
+      try {
+        await server.stop();
+        done();
+      } catch(error) {
+        console.error(error);
+      }
+    } else {
+      done();
+    }
+  });
+
+  describe('start', () => {
+    it('starts a server on a specified port', async done => {
+      try {
+        const port = await server.start();
+        expect(port).toBe(Number(process.env.PORT));
+        done();
+      } catch(error) {
+        console.error(error);
+      }
+    });
+
+    it('responds to requests', async done => {
+      const port = await server.start();
+
+      const url = `http://localhost:${port}/`;
+      request.get(url, (error, response) => {
+        if (error) {
+          done.fail(error);
+        } else {
+          expect(response.statusCode).toBe(200);
+          const body = JSON.parse(response.body);
+          expect(body.message).toContain('ready');
+          done();
+        }
+      });
+    });
+  });
+});
