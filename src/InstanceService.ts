a13ddbf06a5747ffc267387d751296b273273186
Florian Keller <florian.keller@wire.com>
2019-03-07 10:52:54 +0100

    fix linting, remove no-unused-variable

--- src/InstanceService.ts
+++ src/InstanceService.ts
@@ -82,9 +82,9 @@ export interface Instance {
 }
 
 class InstanceService {
-  private cachedInstances: LRUCache<Instance>;
+  private readonly cachedInstances: LRUCache<Instance>;
 
-  constructor(private maximumInstances = 100) {
+  constructor(private readonly maximumInstances = 100) {
     this.cachedInstances = new LRUCache(this.maximumInstances);
   }
 
