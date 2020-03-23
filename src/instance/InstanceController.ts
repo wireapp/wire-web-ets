ac340213f4645ff26547eb93e9303da58821bfad
Panayiotis Lipiridis <lipiridis@gmail.com>
2020-03-23 19:31:04 +0200

    return

--- src/instance/InstanceController.ts
+++ src/instance/InstanceController.ts
@@ -1,5 +1,5 @@
 import {Body, Controller, Put} from '@nestjs/common';
-import {ApiTags} from '@nestjs/swagger';
+import {ApiTags, ApiResponse} from '@nestjs/swagger';
 import {InstanceCreationOptions} from './InstanceCreationOptions';
 import {InstanceService} from './InstanceService';
 import {NewInstanceResponse} from './NewInstanceResponse';
@@ -10,6 +10,9 @@ export class InstanceController {
   constructor(private readonly instanceService: InstanceService) {}
 
   @Put()
+  @ApiResponse({description: 'The instance has successfully created.', status: 200})
+  @ApiResponse({description: 'Validation error.', status: 422})
+  @ApiResponse({description: 'Internal server error.', status: 500})
   async putInstance(@Body() body: InstanceCreationOptions): Promise<NewInstanceResponse> {
     const instanceId = await this.instanceService.createInstance(body);
     return {
