47711264bec86c158d79501ef148b6fd771eaf84
Lipis <lipiridis@gmail.com>
2020-03-24 17:39:55 +0200

    Add get instance (#1255)
    * Add get instance
    
    * Add validation

--- src/instance/InstanceController.ts
+++ src/instance/InstanceController.ts
@@ -1,11 +1,12 @@
-import {Body, Controller, Put, Delete, Param, Res} from '@nestjs/common';
+import {Body, Controller, Put, Delete, Param, Res, Get} from '@nestjs/common';
 import {ApiTags, ApiResponse, ApiOperation} from '@nestjs/swagger';
 import {InstanceCreationOptions} from './InstanceCreationOptions';
 import {InstanceService} from './InstanceService';
 import {NewInstanceResponse} from './NewInstanceResponse';
-import {ErrorMessage} from '../config';
+import {ErrorMessage, ServerErrorMessage} from '../config';
 import * as HTTP_STATUS_CODE from 'http-status-codes';
 import {Response} from 'express';
+import {Validator} from 'class-validator';
 
 @ApiTags('Instance')
 @Controller('instance')
@@ -30,8 +31,18 @@ export class InstanceController {
   @ApiOperation({summary: 'Delete an instance.'})
   @ApiResponse({description: 'The instance has successfully deleted.', status: 200})
   @ApiResponse({description: 'Instance not found', status: 404})
+  @ApiResponse({description: 'Validation error', status: 422})
   @ApiResponse({description: 'Internal server error', status: 500})
   async deleteInstance(@Param('instanceId') instanceId: string, @Res() res: Response): Promise<void> {
+    const validator = new Validator();
+    if (!validator.isUUID(instanceId, '4')) {
+      const errorMessage: ErrorMessage = {
+        code: HTTP_STATUS_CODE.UNPROCESSABLE_ENTITY,
+        error: `Instance ID must me a UUID.`,
+      };
+      res.status(errorMessage.code).json(errorMessage);
+    }
+
     if (!this.instanceService.instanceExists(instanceId)) {
       const errorMessage: ErrorMessage = {
         code: HTTP_STATUS_CODE.NOT_FOUND,
@@ -41,5 +52,47 @@ export class InstanceController {
     }
 
     await this.instanceService.deleteInstance(instanceId);
+  }
+
+  @Get(':instanceId')
+  @ApiOperation({summary: 'Get information about an instance.'})
+  @ApiResponse({description: 'The instance has successfully deleted.', status: 200})
+  @ApiResponse({description: 'Instance not found', status: 404})
+  @ApiResponse({description: 'Validation error', status: 422})
+  @ApiResponse({description: 'Internal server error', status: 500})
+  async getInstance(@Param('instanceId') instanceId: string, @Res() res: Response): Promise<void> {
+    const validator = new Validator();
+    if (!validator.isUUID(instanceId, '4')) {
+      const errorMessage: ErrorMessage = {
+        code: HTTP_STATUS_CODE.UNPROCESSABLE_ENTITY,
+        error: `Instance ID must me a UUID.`,
+      };
+      res.status(errorMessage.code).json(errorMessage);
+    }
+
+    if (!this.instanceService.instanceExists(instanceId)) {
+      const errorMessage: ErrorMessage = {
+        code: HTTP_STATUS_CODE.NOT_FOUND,
+        error: `Instance "${instanceId}" not found.`,
+      };
+      res.status(errorMessage.code).json(errorMessage);
+    }
+
+    try {
+      const instance = this.instanceService.getInstance(instanceId);
+      res.json({
+        backend: instance.backendType.name,
+        clientId: instance.client.context!.clientId,
+        instanceId,
+        name: instance.name,
+      });
+    } catch (error) {
+      const errorMessage: ServerErrorMessage = {
+        code: HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR,
+        error: error.message,
+        stack: error.stack,
+      };
+      res.status(errorMessage.code).json(errorMessage);
+    }
   }
 }
