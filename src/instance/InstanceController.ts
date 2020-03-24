00dcdd6206f815c0b7a418654cdfafbff374c5fc
Lipis <lipiridis@gmail.com>
2020-03-24 19:03:04 +0200

    Archive conversation (#1256)

--- src/instance/InstanceController.ts
+++ src/instance/InstanceController.ts
@@ -1,12 +1,12 @@
-import {Body, Controller, Put, Delete, Param, Res, Get} from '@nestjs/common';
-import {ApiTags, ApiResponse, ApiOperation} from '@nestjs/swagger';
+import {Body, Controller, Delete, Get, Param, Post, Put, Res} from '@nestjs/common';
+import {ApiOperation, ApiResponse, ApiTags} from '@nestjs/swagger';
+import {Validator} from 'class-validator';
+import {Response} from 'express';
+import * as HTTP_STATUS_CODE from 'http-status-codes';
+import {ErrorMessage, ServerErrorMessage} from '../config';
+import {InstanceArchiveOptions} from './InstanceArchiveOptions';
 import {InstanceCreationOptions} from './InstanceCreationOptions';
 import {InstanceService} from './InstanceService';
-import {NewInstanceResponse} from './NewInstanceResponse';
-import {ErrorMessage, ServerErrorMessage} from '../config';
-import * as HTTP_STATUS_CODE from 'http-status-codes';
-import {Response} from 'express';
-import {Validator} from 'class-validator';
 
 @ApiTags('Instance')
 @Controller('instance')
@@ -19,12 +19,12 @@ export class InstanceController {
   @ApiResponse({description: 'Bad request', status: 400})
   @ApiResponse({description: 'Validation error', status: 422})
   @ApiResponse({description: 'Internal server error', status: 500})
-  async putInstance(@Body() body: InstanceCreationOptions): Promise<NewInstanceResponse> {
+  async putInstance(@Body() body: InstanceCreationOptions, @Res() res: Response): Promise<void> {
     const instanceId = await this.instanceService.createInstance(body);
-    return {
+    res.status(HTTP_STATUS_CODE.OK).json({
       instanceId,
       name: body.name || '',
-    };
+    });
   }
 
   @Delete(':instanceId')
@@ -85,6 +85,50 @@ export class InstanceController {
         clientId: instance.client.context!.clientId,
         instanceId,
         name: instance.name,
+      });
+    } catch (error) {
+      const errorMessage: ServerErrorMessage = {
+        code: HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR,
+        error: error.message,
+        stack: error.stack,
+      };
+      res.status(errorMessage.code).json(errorMessage);
+    }
+  }
+
+  @Post(':instanceId/archive/')
+  @ApiOperation({summary: 'Archive a conversation.'})
+  @ApiResponse({description: 'The conversation archived status has been updated.', status: 200})
+  @ApiResponse({description: 'Instance not found', status: 404})
+  @ApiResponse({description: 'Validation error', status: 422})
+  @ApiResponse({description: 'Internal server error', status: 500})
+  async arvhiveConversation(
+    @Param('instanceId') instanceId: string,
+    @Body() body: InstanceArchiveOptions,
+    @Res() res: Response,
+  ): Promise<void> {
+    const validator = new Validator();
+    if (!validator.isUUID(instanceId, '4')) {
+      const errorMessage: ErrorMessage = {
+        code: HTTP_STATUS_CODE.UNPROCESSABLE_ENTITY,
+        error: `Instance ID must me a UUID.`,
+      };
+      res.status(errorMessage.code).json(errorMessage);
+    }
+
+    if (!this.instanceService.instanceExists(instanceId)) {
+      const errorMessage: ErrorMessage = {
+        code: HTTP_STATUS_CODE.NOT_FOUND,
+        error: `Instance "${instanceId}" not found.`,
+      };
+      res.status(errorMessage.code).json(errorMessage);
+    }
+
+    try {
+      const instanceName = await this.instanceService.toggleArchiveConversation(instanceId, body);
+      res.status(HTTP_STATUS_CODE.OK).json({
+        instanceId,
+        name: instanceName,
       });
     } catch (error) {
       const errorMessage: ServerErrorMessage = {
