956b59e53128c67846e92d1ee30eeb7fa4a9803f
Panayiotis Lipiridis <lipiridis@gmail.com>
2020-03-25 14:13:39 +0200

    Is not emptu

--- src/instance/InstanceAvailiabilityOptions.ts
+++ src/instance/InstanceAvailiabilityOptions.ts
@@ -1,12 +1,11 @@
 import {ApiProperty} from '@nestjs/swagger';
-import {IsNotEmpty, IsUUID, IsEnum} from 'class-validator';
 import {Availability} from '@wireapp/protocol-messaging';
+import {IsEnum, IsUUID} from 'class-validator';
 
 export class InstanceAvailiabilityOptions {
   @ApiProperty({
     enum: [Availability.Type.AVAILABLE, Availability.Type.AWAY, Availability.Type.BUSY, Availability.Type.NONE],
   })
-  @IsNotEmpty()
   @IsEnum(Availability.Type)
   type!: Availability.Type;
 
