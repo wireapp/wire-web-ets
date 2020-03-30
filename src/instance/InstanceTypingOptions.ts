7c94da17fd10260a73a03faf407115ce965fb98f
Lipis <lipiridis@gmail.com>
2020-03-30 19:46:27 +0300

    feat: Add /sendSessionReset and /sendTyping endpoint (#1304)
    * feat: Add /sendPing endpoint
    
    * feat: Add /sendButtonAction endpoint
    
    * variable
    
    * Add /sendButtonActionConfirmation
    
    * feat: Add /sendReaction endpoint
    
    * feat: Add /sendSessionReset endpoint
    
    * feat: Add /sendTyping endpoint

---
+++ src/instance/InstanceTypingOptions.ts
@@ -0,0 +1,15 @@
+import {ApiProperty} from '@nestjs/swagger';
+import {CONVERSATION_TYPING} from '@wireapp/api-client/dist/conversation/data/';
+import {IsEnum, IsUUID} from 'class-validator';
+
+export class InstanceTypingOptions {
+  @ApiProperty()
+  @IsUUID('4')
+  conversationId!: string;
+
+  @ApiProperty({
+    enum: [CONVERSATION_TYPING.STARTED, CONVERSATION_TYPING.STOPPED],
+  })
+  @IsEnum(CONVERSATION_TYPING)
+  status!: CONVERSATION_TYPING;
+}
