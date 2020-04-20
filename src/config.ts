012e4c24e7a23694efaaf296e79d489f4d15525b
Panayiotis Lipiridis <lipiridis@gmail.com>
2020-04-20 15:26:25 +0300

    chore: Cleanup

--- src/config.ts
+++ src/config.ts
@@ -18,22 +18,8 @@
  */
 
 import * as dotenv from 'dotenv';
-import * as path from 'path';
-
-const {version}: {version: string} = require('../package.json');
 
 dotenv.config();
-
-export interface ServerConfig {
-  CACHE_DURATION_SECONDS: number;
-  COMPRESS_LEVEL: number;
-  COMPRESS_MIN_SIZE: number;
-  DEVELOPMENT?: boolean;
-  DIST_DIR: string;
-  ENVIRONMENT: string;
-  PORT_HTTP: number;
-  VERSION: string;
-}
 
 export interface ErrorMessage {
   code: number;
@@ -43,17 +29,3 @@ export interface ErrorMessage {
 export interface ServerErrorMessage extends ErrorMessage {
   stack?: string;
 }
-
-const config: ServerConfig = {
-  CACHE_DURATION_SECONDS: 300, // 5 minutes
-  COMPRESS_LEVEL: 6,
-  COMPRESS_MIN_SIZE: 500,
-  DIST_DIR: path.resolve(__dirname),
-  ENVIRONMENT: process.env.ENVIRONMENT || 'prod',
-  PORT_HTTP: Number(process.env.PORT) || 21080,
-  VERSION: version,
-};
-
-config.DEVELOPMENT = config.ENVIRONMENT === 'dev';
-
-export {config};
